0bc468d077918db8950eafe8ffd22810651881c4
Bruno Rodrigues <bruno@brodrigues.co>
2025-07-10 10:34:41 +0200

    separated user defined functions

--- _targets.R
+++ _targets.R
@@ -20,27 +20,6 @@ tar_option_set(
   )
 )
 
-get_domain_name <- function(df){
-  filter(df, i == 1, type == "domain") %>%
-    pull(display_name)
-}
-
-get_subfield_name <- function(df){
-  filter(df, i == 1, type == "subfield") %>%
-    pull(display_name)
-}
-
-safe_get_domain_name <- purrr::possibly(get_domain_name, otherwise = "MISSING-DOMAIN")
-safe_get_subfield_name <- purrr::possibly(get_subfield_name, otherwise = "MISSING-SUBFIELD")
-
-get_first_author_country <- function(df){
-  filter(df, author_position == "first") %>%
-    pull(affiliations) %>%
-    map(bind_rows) %>%
-    map(\(x)(pull(x, country_code)))
-}
-
-
 list(
   tar_target(
     luxembourg_works_path,
@@ -55,24 +34,24 @@ list(
   tar_target(
     openalex_1,
     luxembourg_works_raw %>%
-    mutate(
-      publication_year = year(publication_date),
-      doi_missing = is.na(doi)
-    ) %>%
-    filter(
-      year(publication_date) >= 2015
-    )
+      mutate(
+        publication_year = year(publication_date),
+        doi_missing = is.na(doi)
+      ) %>%
+      filter(
+        year(publication_date) >= 2015
+      )
   ),
 
   tar_target(
     type_doi_missing,
     tabyl(openalex_1, type, doi_missing) %>%
-    mutate(Total = `FALSE`+`TRUE`) %>%
-    rename(
-      Type = type,
-      `Has DOI` = `FALSE`,
-      `DOI missing` = `TRUE`
-    )
+      mutate(Total = `FALSE` + `TRUE`) %>%
+      rename(
+        Type = type,
+        `Has DOI` = `FALSE`,
+        `DOI missing` = `TRUE`
+      )
   ),
 
   tar_target(
@@ -81,26 +60,28 @@ list(
       openalex_1,
       type == "article"
     ) %>%
-    mutate(
-      first_author_country = map(authorships, get_first_author_country),
-      is_lu_first_author = map_lgl(first_author_country, \(x)(grepl("LU", x))),
-      primary_domain_name = map_chr(topics, safe_get_domain_name),
-      primary_subfield_name = map_chr(topics, safe_get_subfield_name)
-    )
+      mutate(
+        first_author_country = map(authorships, get_first_author_country),
+        is_lu_first_author = map_lgl(first_author_country, \(x) {
+          (grepl("LU", x))
+        }),
+        primary_domain_name = map_chr(topics, safe_get_domain_name),
+        primary_subfield_name = map_chr(topics, safe_get_subfield_name)
+      )
   ),
 
   tar_target(
     lu_first_authors,
     dataset %>%
-    group_by(publication_year, is_lu_first_author) %>%
-    summarise(total = n_distinct(doi))
+      group_by(publication_year, is_lu_first_author) %>%
+      summarise(total = n_distinct(doi))
   ),
 
   tar_target(
     primary_domain_lu,
     dataset %>%
-    group_by(publication_year, primary_domain_name, is_lu_first_author) %>%
-    summarise(total = n_distinct(doi)) 
+      group_by(publication_year, primary_domain_name, is_lu_first_author) %>%
+      summarise(total = n_distinct(doi))
     #rename(
     #  `Primary domain name` = primary_domain_name,
     #  `Not LU-affiliated first author` = `FALSE`,
@@ -122,15 +103,15 @@ list(
     path = "report/report.qmd"
   )
 
-                                        #tar_target(
-                                        #  openalex_2,
-                                        #  openalex_1 %>%
-                                        #  mutate(domain_name = map(topics, get_domain_name))
-                                        #)
+  #tar_target(
+  #  openalex_2,
+  #  openalex_1 %>%
+  #  mutate(domain_name = map(topics, get_domain_name))
+  #)
 
   #
   #                                      #Need to add the Domain
-  #                                      #luxembourg_works %>% head(1)  %>% pull(topics) %>% .[[1]] %>%   filter(i == 1, type == "domain") %>% pull(display_name) 
+  #                                      #luxembourg_works %>% head(1)  %>% pull(topics) %>% .[[1]] %>%   filter(i == 1, type == "domain") %>% pull(display_name)
 
   #tar_target(
   #  authorships_topics_df,
@@ -142,7 +123,7 @@ list(
   #  select(publication_year, topics, authorships_affiliations) %>%
   #  unnest(authorships_affiliations) %>%
   #  count(publication_year, topics, country_code)
-  #  
+  #
   #),
 
   #tar_target(
@@ -153,5 +134,4 @@ list(
   #    reframe(authors_countries = unlist(authors_countries)) %>%
   #    count(publication_year, authors_countries)
   #)
-
 )
---
+++ functions.R
@@ -0,0 +1,26 @@
+get_domain_name <- function(df) {
+  filter(df, i == 1, type == "domain") %>%
+    pull(display_name)
+}
+
+safe_get_domain_name <- purrr::possibly(
+  get_domain_name,
+  otherwise = "MISSING-DOMAIN"
+)
+
+get_subfield_name <- function(df) {
+  filter(df, i == 1, type == "subfield") %>%
+    pull(display_name)
+}
+
+safe_get_subfield_name <- purrr::possibly(
+  get_subfield_name,
+  otherwise = "MISSING-SUBFIELD"
+)
+
+get_first_author_country <- function(df) {
+  filter(df, author_position == "first") %>%
+    pull(affiliations) %>%
+    map(bind_rows) %>%
+    map(\(x) (pull(x, country_code)))
+}
