db8c4a5e8d7eb48dd8c348ae258c87143cd35082
Bruno Rodrigues <bruno@brodrigues.co>
2025-07-10 10:34:51 +0200

    added gen-env.R

---
+++ gen-env.R
@@ -0,0 +1,52 @@
+# This script uses the {rix} package to generate a 'default.nix' file,
+# which defines a reproducible environment for the pipeline.
+
+library(rix)
+
+# Define the execution environment for the pipeline.
+# This includes all necessary R packages, Quarto for rendering,
+# and pulls the latest versions of {rix} and {rixpress} from GitHub.
+rix(
+  date = "2025-07-07",
+  r_pkgs = c(
+    "R_utils",
+    "archive",
+    "crosstalk",
+    "dplyr",
+    "DT",
+    "ggplot2",
+    "httr",
+    "htmltools",
+    "janitor",
+    "jsonlite",
+    "lubridate",
+    "openalexR",
+    "plotly",
+    "purrr",
+    "quarto",
+    "readr",
+    "readxl",
+    "rix",
+    "tarchetypes",
+    "targets",
+    "tidyjson",
+    "tidyr",
+    "tinytable",
+    "xml2"
+  ),
+  git_pkgs = list(
+    list(
+      package_name = "rixpress",
+      repo_url = "https://github.com/b-rodrigues/rixpress",
+      commit = "dfef00af24b43aabbcf26efe61b3fb72d4a89bd9"
+    )
+  ),
+  system_pkgs = c(
+    "air-formatter",
+    "quarto",
+    "typst"
+  ),
+  ide = "none",
+  project_path = ".",
+  overwrite = TRUE
+)
