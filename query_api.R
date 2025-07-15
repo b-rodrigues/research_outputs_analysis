10c090d39a9795c3334e6b57fe5231936d2c9ae6
Bruno Rodrigues <bruno@brodrigues.co>
2025-07-15 15:54:45 +0200

    updated data

--- query_api.R
+++ query_api.R
@@ -7,27 +7,30 @@ luxembourg_works <- oa_fetch(
   # Filter for works with at least one author affiliated with Luxembourg
   authorships.institutions.country_code = "LU",
   # Select the fields you requested
-  options = list(select = c(
-    "id",
-    "doi",
-    "title",
-    "publication_date",
-    "type",
-    "primary_topic",
-    "topics", # The first item in this list is the primary topic
-    "keywords",
-    "sustainable_development_goals",
-    "language",
-    "primary_location",
-    "cited_by_count",
-    "counts_by_year",
-    "open_access",
-    "authorships"
-  )),
+  options = list(
+    select = c(
+      "id",
+      "doi",
+      "title",
+      "publication_date",
+      "type",
+      "primary_topic",
+      "topics", # The first item in this list is the primary topic
+      "keywords",
+      "sustainable_development_goals",
+      "language",
+      "primary_location",
+      "cited_by_count",
+      "citation_normalized_percentile", # Field-normalized citation percentile (0-100)
+      "counts_by_year",
+      "open_access",
+      "authorships"
+    )
+  ),
   # Set a reasonable limit (adjust as needed)
   count_only = FALSE,
   verbose = TRUE
 )
 
 # Need to save as RDS because of list columns of dfs
-saveRDS(luxembourg_works, "dataset/luxembourg_works.rds")
+saveRDS(luxembourg_works, "dataset/luxembourg_works_2025_07_15.rds")
